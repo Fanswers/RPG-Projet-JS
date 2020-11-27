a63f8ecf8e42a2961ef4491857ba861952c9320e
Jucrz <jujucruz99@outlook.fr>
2020-11-27 11:00:15 +0100

    :sparkles:
    Add class entité

---
+++ Class/Entity.js
@@ -0,0 +1,10 @@
+class Entity {
+    constructor(Pv, Defense, Vitesse, Esquive, Attaque, Pm) {
+      this.Pv = Pv;
+      this.Defense = Defense;
+      this.Vitesse = Vitesse;
+      this.Esquive = Esquive;
+      this.Attaque = Attaque;
+      this.Pm = Pm;
+    }
+  }
