5e9fdccc805a8cbec89e167adc5a171132f11c97
Larrode Alexis <alexis.larrode@gmail.com>
2020-12-02 22:19:02 +0100

    :sparkles: Add create player
    Ajout du formulaire de création du personnage + Fonction récuperation du formulaire et création du joueur

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 import { Personnage } from './Class/Personnage.js'
 import { Monster } from './Class/Monster.js'
 
+//Recupération des données du formulaire
+$("#sendPlayer").click(function(e){
+    e.preventDefault();
+    let playerInfo = $("#playerForm").serializeArray();
+    game(playerInfo)
+})
+
+function game(playerInfo){
+    //Creation du joueur
+    let player = new Personnage(playerInfo[1]['value'], playerInfo[0]['value']) 
+}
