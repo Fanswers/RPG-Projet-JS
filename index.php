7e2d78084ddc6bd4c0d2fa1fc97f0f81ef39a156
Larrode Alexis <alexis.larrode@gmail.com>
2020-12-03 19:11:22 +0100

    :recycle: index.php
    Modification formulaire création de partie

--- index.php
+++ index.php
@@ -28,6 +28,30 @@
         </p>
         <button id="sendPlayer">Jouer</button>
     </form>
+    <div id="formDiv" class="cacher">
+        <h1>Nouvelle partie</h1>
+        <form id="playerForm">
+            <p>
+                Pseudo
+                <input type="text" name="name" id="playerName">
+            </p>
+            <P>
+                <input type="radio" name="class" value="Warrior"> Warrior
+            </p>
+            <P>
+                <input type="radio" name="class" value="Mage"> Mage
+            </p>
+            <P>
+                <input type="radio" name="class" value="Rogue"> Rogue
+            </p>
+            <P>
+                <input type="radio" name="class" value="Archer"> Archer
+            </p>
+            <button id="sendPlayer">Jouer</button>
+        </form>
+    </div>
+
+    
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script type="module" src="main.js"></script>
