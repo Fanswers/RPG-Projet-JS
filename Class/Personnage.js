b21707fadb26a1bdb581fa1aedbadb6ce6b25d31
Larrode Alexis <alexis.larrode@gmail.com>
2020-12-03 19:10:04 +0100

    :recycle: class Personnage
    Ajout des données PvMax et PmMax à la création du personnage

--- Class/Personnage.js
+++ Class/Personnage.js
@@ -7,44 +7,52 @@ export class Personnage extends Entity{
         {
             case "Warrior":
                 this.Pv = 140;
+                this.PvMax = 140;
                 this.Attaque = 40;
                 this.Defense = 30;
                 this.Vitesse = 10;
                 this.Esquive = 2;
                 this.Pm = 10;
+                this.PmMax = 10;
                 this.Crit = 5;
                 this.Gold = 0;
                 break;
 
             case "Mage":
                 this.Pv = 100;
+                this.PvMax = 140
                 this.Attaque = 40;
                 this.Defense = 30;
                 this.Vitesse = 10;
                 this.Esquive = 2;
                 this.Pm = 10;
+                this.PmMax = 10;
                 this.Crit = 5;
                 this.Gold = 0;
                 break;
 
             case "Rogue":
                 this.Pv = 100;
+                this.PvMax = 140;
                 this.Attaque = 40;
                 this.Defense = 30;
                 this.Vitesse = 10;
                 this.Esquive = 2;
                 this.Pm = 10;
+                this.PmMax = 10;
                 this.Crit = 5;
                 this.Gold = 0;
                 break;
 
             case "Archer":
                 this.Pv = 100;
+                this.PvMax = 140;
                 this.Attaque = 40;
                 this.Defense = 30;
                 this.Vitesse = 10;
                 this.Esquive = 2;
                 this.Pm = 10;
+                this.PmMax = 10;
                 this.Crit = 5;
                 this.Gold = 0;
                 break;
