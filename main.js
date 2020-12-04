80fd6f696e76a951d2b98905f1bf51921c8dbdd1
Jucrz <jujucruz99@outlook.fr>
2020-12-04 01:00:19 +0100

    Ajout des compétences de toutes les classes

--- main.js
+++ main.js
@@ -253,6 +253,336 @@ $("#attaque4Warrior").click(function () {
     FinDeCombat()
 })
 
+// Fonction au clique du bouton qui lance l'attaque séléctionnée du Mage
+$("#attaque1Mage").click(function () {
+    if (player.Vitesse > monster.Vitesse) {
+        $("#messages").append(`<p>Vous lancez ${$("#attaque1" + player.Type).val()} !</p>`);
+        monster.Pv -= player.Attaque;
+        $("#messages").append(`<p>Le monstre a perdu ${player.Attaque} points de vie !</p>`);
+        if (monster.Pv > 0) {
+            $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+            player.Pv -= monster.Attaque;
+            $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+        }
+    } else {
+        $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+        player.Pv -= monster.Attaque;
+        $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+        if (player.Pv > 0) {
+            $("#messages").append(`<p>Vous lancez ${$("#attaque1" + player.Type).val()} !</p>`);
+            monster.Pv -= player.Attaque;
+            $("#messages").append(`<p>Le monstre a perdu ${player.Attaque} point de vie !</p>`);
+        }
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+
+    FinDeCombat()
+})
+
+$("#attaque2Mage").click(function () {
+    if (player.Pm > 3) {
+        player.Pm -= 3;
+        if (player.Vitesse > monster.Vitesse) {
+            $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+            $("#messages").append(`<p>Vous avez perdu 3 pm !</p>`);
+            monster.Pv -= player.Attaque * 1.5;
+            $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 1.5} points de vie !</p>`);
+            if (monster.Pv > 0) {
+                $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+                player.Pv -= monster.Attaque;
+                $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            }
+        } else {
+            $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+            player.Pv -= monster.Attaque;
+            $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            if (player.Pv > 0) {
+                $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+                $("#messages").append(`<p>Vous avez perdu 3 pm !</p>`);
+                monster.Pv -= player.Attaque * 1.5;
+                $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 1.5} points de vie !</p>`);
+            }
+        }
+    } else {
+        $("#messages").append(`<p>Vous n'avez pas assez de pm !</p>`);
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+    FinDeCombat()
+})
+
+$("#attaque3Mage").click(function () {
+    if (player.Pm > 6) {
+        player.Pm -= 6;
+        if (player.Vitesse > monster.Vitesse) {
+            $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+            $("#messages").append(`<p>Vous avez perdu 6 pm !</p>`);
+            monster.Pv -= player.Attaque * 2;
+            $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 2} points de vie !</p>`);
+            if (monster.Pv > 0) {
+                $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+                player.Pv -= monster.Attaque;
+                $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            }
+        } else {
+            $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+            player.Pv -= monster.Attaque;
+            $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            if (player.Pv > 0) {
+                $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+                $("#messages").append(`<p>Vous avez perdu 6 pm !</p>`);
+                monster.Pv -= player.Attaque * 3;
+                $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 2} points de vie !</p>`);
+            }
+        }
+    } else {
+        $("#messages").append(`<p>Vous n'avez pas assez de pm !</p>`);
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+    FinDeCombat()
+})
+
+$("#attaque4Mage").click(function () {
+    if (monster.Attaque > player.Defense) {
+        $("#messages").append(`<p>Vous vous mettez en position défensive.</p>`);
+        player.Pv -= monster.Attaque - player.Defense;
+        $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque - player.Defense} points de vie !</p>`);
+    } else {
+        $("#messages").append(`<p>Vous vous mettez en position défensive.</p>`);
+        player.Pv -= 0;
+        $("#messages").append(`<p>Vous avez perdu 0 point de vie.</p>`);
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+    FinDeCombat()
+})
+
+// Fonction au clique du bouton qui lance l'attaque séléctionnée du Rogue
+$("#attaque1Rogue").click(function () {
+    if (player.Vitesse > monster.Vitesse) {
+        $("#messages").append(`<p>Vous lancez ${$("#attaque1" + player.Type).val()} !</p>`);
+        monster.Pv -= player.Attaque;
+        $("#messages").append(`<p>Le monstre a perdu ${player.Attaque} points de vie !</p>`);
+        if (monster.Pv > 0) {
+            $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+            player.Pv -= monster.Attaque;
+            $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+        }
+    } else {
+        $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+        player.Pv -= monster.Attaque;
+        $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+        if (player.Pv > 0) {
+            $("#messages").append(`<p>Vous lancez ${$("#attaque1" + player.Type).val()} !</p>`);
+            monster.Pv -= player.Attaque;
+            $("#messages").append(`<p>Le monstre a perdu ${player.Attaque} point de vie !</p>`);
+        }
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+
+    FinDeCombat()
+})
+
+$("#attaque2Rogue").click(function () {
+    if (player.Pm > 3) {
+        player.Pm -= 3;
+        if (player.Vitesse > monster.Vitesse) {
+            $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+            $("#messages").append(`<p>Vous avez perdu 3 pm !</p>`);
+            monster.Pv -= player.Attaque * 1.5;
+            $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 1.5} points de vie !</p>`);
+            if (monster.Pv > 0) {
+                $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+                player.Pv -= monster.Attaque;
+                $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            }
+        } else {
+            $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+            player.Pv -= monster.Attaque;
+            $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            if (player.Pv > 0) {
+                $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+                $("#messages").append(`<p>Vous avez perdu 3 pm !</p>`);
+                monster.Pv -= player.Attaque * 1.5;
+                $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 1.5} points de vie !</p>`);
+            }
+        }
+    } else {
+        $("#messages").append(`<p>Vous n'avez pas assez de pm !</p>`);
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+    FinDeCombat()
+})
+
+$("#attaque3Rogue").click(function () {
+    if (player.Pm > 6) {
+        player.Pm -= 6;
+        if (player.Vitesse > monster.Vitesse) {
+            $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+            $("#messages").append(`<p>Vous avez perdu 6 pm !</p>`);
+            monster.Pv -= player.Attaque * 2;
+            $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 2} points de vie !</p>`);
+            if (monster.Pv > 0) {
+                $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+                player.Pv -= monster.Attaque;
+                $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            }
+        } else {
+            $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+            player.Pv -= monster.Attaque;
+            $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            if (player.Pv > 0) {
+                $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+                $("#messages").append(`<p>Vous avez perdu 6 pm !</p>`);
+                monster.Pv -= player.Attaque * 3;
+                $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 2} points de vie !</p>`);
+            }
+        }
+    } else {
+        $("#messages").append(`<p>Vous n'avez pas assez de pm !</p>`);
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+    FinDeCombat()
+})
+
+$("#attaque4Rogue").click(function () {
+    if (monster.Attaque > player.Defense) {
+        $("#messages").append(`<p>Vous vous mettez en position défensive.</p>`);
+        player.Pv -= monster.Attaque - player.Defense;
+        $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque - player.Defense} points de vie !</p>`);
+    } else {
+        $("#messages").append(`<p>Vous vous mettez en position défensive.</p>`);
+        player.Pv -= 0;
+        $("#messages").append(`<p>Vous avez perdu 0 point de vie.</p>`);
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+    FinDeCombat()
+})
+
+// Fonction au clique du bouton qui lance l'attaque séléctionnée du Archer
+$("#attaque1Archer").click(function () {
+    if (player.Vitesse > monster.Vitesse) {
+        $("#messages").append(`<p>Vous lancez ${$("#attaque1" + player.Type).val()} !</p>`);
+        monster.Pv -= player.Attaque;
+        $("#messages").append(`<p>Le monstre a perdu ${player.Attaque} points de vie !</p>`);
+        if (monster.Pv > 0) {
+            $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+            player.Pv -= monster.Attaque;
+            $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+        }
+    } else {
+        $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+        player.Pv -= monster.Attaque;
+        $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+        if (player.Pv > 0) {
+            $("#messages").append(`<p>Vous lancez ${$("#attaque1" + player.Type).val()} !</p>`);
+            monster.Pv -= player.Attaque;
+            $("#messages").append(`<p>Le monstre a perdu ${player.Attaque} point de vie !</p>`);
+        }
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+
+    FinDeCombat()
+})
+
+$("#attaque2Archer").click(function () {
+    if (player.Pm > 3) {
+        player.Pm -= 3;
+        if (player.Vitesse > monster.Vitesse) {
+            $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+            $("#messages").append(`<p>Vous avez perdu 3 pm !</p>`);
+            monster.Pv -= player.Attaque * 1.5;
+            $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 1.5} points de vie !</p>`);
+            if (monster.Pv > 0) {
+                $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+                player.Pv -= monster.Attaque;
+                $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            }
+        } else {
+            $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+            player.Pv -= monster.Attaque;
+            $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            if (player.Pv > 0) {
+                $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+                $("#messages").append(`<p>Vous avez perdu 3 pm !</p>`);
+                monster.Pv -= player.Attaque * 1.5;
+                $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 1.5} points de vie !</p>`);
+            }
+        }
+    } else {
+        $("#messages").append(`<p>Vous n'avez pas assez de pm !</p>`);
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+    FinDeCombat()
+})
+
+$("#attaque3Archer").click(function () {
+    if (player.Pm > 6) {
+        player.Pm -= 6;
+        if (player.Vitesse > monster.Vitesse) {
+            $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+            $("#messages").append(`<p>Vous avez perdu 6 pm !</p>`);
+            monster.Pv -= player.Attaque * 2;
+            $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 2} points de vie !</p>`);
+            if (monster.Pv > 0) {
+                $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+                player.Pv -= monster.Attaque;
+                $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            }
+        } else {
+            $("#messages").append(`<p>Le monstre vous attaque !</p>`);
+            player.Pv -= monster.Attaque;
+            $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque} points de vie !</p>`);
+            if (player.Pv > 0) {
+                $("#messages").append(`<p>Vous lancez ${$("#attaque2" + player.Type).val()} !</p>`);
+                $("#messages").append(`<p>Vous avez perdu 6 pm !</p>`);
+                monster.Pv -= player.Attaque * 3;
+                $("#messages").append(`<p>Le monstre a perdu ${player.Attaque * 2} points de vie !</p>`);
+            }
+        }
+    } else {
+        $("#messages").append(`<p>Vous n'avez pas assez de pm !</p>`);
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+    FinDeCombat()
+})
+
+$("#attaque4Archer").click(function () {
+    if (monster.Attaque > player.Defense) {
+        $("#messages").append(`<p>Vous vous mettez en position défensive.</p>`);
+        player.Pv -= monster.Attaque - player.Defense;
+        $("#messages").append(`<p>Vous avez perdu  ${monster.Attaque - player.Defense} points de vie !</p>`);
+    } else {
+        $("#messages").append(`<p>Vous vous mettez en position défensive.</p>`);
+        player.Pv -= 0;
+        $("#messages").append(`<p>Vous avez perdu 0 point de vie.</p>`);
+    }
+    $("#messages").append(`<p>------------------------------------</p>`)
+    $("#messages" + player.Type).toggleClass("cacher");
+    $("#messages" + player.Type).toggleClass("cacher");
+    FinDeCombat()
+})
+
 function refreshPlayerData() {
     $("#pv").text(player.Pv);
     $("#pm").text(player.Pm);
