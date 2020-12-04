bef2704619052c786310833c30a0f0fcb36cb336
Larrode Alexis <alexis.larrode@gmail.com>
2020-12-04 01:10:47 +0100

    :recycle: Class Monster

--- Class/Monster.js
+++ Class/Monster.js
@@ -5,55 +5,44 @@ export class Monster extends Entity {
     super(Type, Name, Pv, Defense, Vitesse, Esquive, Attaque, Pm, Crit, Gold);
     switch (Type) {
       case 'Glout':
-        this.Pv = 100;
+        this.Pv = 10;
         this.Defense = 5;
         this.Vitesse = 50;
         this.Esquive = 0, 3;
-        this.Attaque = 25;
+        this.Attaque = 15;
         this.Pm = 15;
         this.Crit = 0, 1;
-        this.Gold = 2;
-        break;
-
-      case 'Groco':
-        this.Pv = 100;
-        this.Defense = 5;
-        this.Vitesse = 50;
-        this.Esquive = 0, 3;
-        this.Attaque = 25;
-        this.Pm = 15;
-        this.Crit = 0, 1;
-        this.Gold = 2;
+        this.Gold = 4;
         break;
 
       case 'Tankse':
-        this.Pv = 100;
+        this.Pv = 80;
         this.Defense = 5;
         this.Vitesse = 50;
         this.Esquive = 0, 3;
-        this.Attaque = 25;
+        this.Attaque = 10;
         this.Pm = 15;
         this.Crit = 0, 1;
-        this.Gold = 2;
+        this.Gold = 7;
         break;
+
+        case 'Groco':
+            this.Pv = 10;
+            this.Defense = 5;
+            this.Vitesse = 50;
+            this.Esquive = 0, 3;
+            this.Attaque = 45;
+            this.Pm = 15;
+            this.Crit = 0, 1;
+            this.Gold = 9;
+            break;
 
       case 'Noxpul':
-        this.Pv = 100;
+        this.Pv = 180;
         this.Defense = 5;
         this.Vitesse = 50;
         this.Esquive = 0, 3;
-        this.Attaque = 25;
-        this.Pm = 15;
-        this.Crit = 0, 1;
-        this.Gold = 2;
-        break;
-
-      case 'Sacdor':
-        this.Pv = 100;
-        this.Defense = 5;
-        this.Vitesse = 50;
-        this.Esquive = 0, 3;
-        this.Attaque = 25;
+        this.Attaque = 55;
         this.Pm = 15;
         this.Crit = 0, 1;
         this.Gold = 2;
