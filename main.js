b19be029a72be1fa7ccf9e5ba5de82998350db81
Larrode Alexis <alexis.larrode@gmail.com>
2020-12-07 23:36:06 +0100

    :sparkles: death player delete
    Supprime la base de donnée quand le joueur est mort

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ $("#start").click(function () {
             }
             else {
                 $("#loadSave").toggleClass("cacher");
+                $("#loadGameDisplay").toggleClass("cacher");
             }
         }
     })
@@ -657,9 +658,18 @@ $("#defButton").click(function () {
 })
 
 function autoSave() {
-    $.ajax({
-        url: 'savePlayer.php',
-        method: 'POST',
-        data: player
-    })
+    if(player.Pv > 0){
+        $.ajax({
+            url: 'savePlayer.php',
+            method: 'POST',
+            data: player
+        })
+    }
+    else{
+        $.ajax({
+            url: 'savePlayer.php',
+            method: 'POST',
+            data: null
+        })
+    }
 }
